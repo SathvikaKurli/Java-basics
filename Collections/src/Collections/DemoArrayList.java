a95b079c11d0e3dca7884fe5c551751904e4ef22
SathvikaKurli <92846635+SathvikaKurli@users.noreply.github.com>
2021-10-27 17:12:41 +0530

    Collections

---
+++ Collections/src/Collections/DemoArrayList.java
@@ -0,0 +1,13 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoArrayList {
+	public static void main(String args[]) {
+		List names = new ArrayList<String>();
+		names.add("Sathvika");
+		names.add(30);
+		System.out.println(names);
+	}
+
+}
---
+++ Collections/src/Collections/DemoArrayList1.java
@@ -0,0 +1,16 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoArrayList1 {
+	public static void main(String args[]) {
+		List<String> names = new ArrayList<String>();
+		names.add("Sathvika");
+		names.add("Kurli");
+		System.out.println(names);
+	}
+
+}
+
+
+
