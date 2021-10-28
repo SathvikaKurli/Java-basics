bd3c37a44489233368fc0b8fc70a22668aba299a
SathvikaKurli <92846635+SathvikaKurli@users.noreply.github.com>
2021-10-28 12:56:29 +0530

    Controlstatements

---
+++ DXC/src/ControlStatements/Student3.java
@@ -0,0 +1,19 @@
+package ControlStatements;
+
+public class Student3 implements Cloneable {
+	public static void main(String[] args) {  
+		int num = 2;  
+		switch (num){  
+		case 0:  
+		System.out.println("number is 0");  
+		break;  
+		case 1:  
+		System.out.println("number is 1");  
+		break;  
+		default:  
+		System.out.println(num);  
+		}  
+		}  
+		}  
+
+
