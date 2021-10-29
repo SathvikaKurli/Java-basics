e681bbd1121b84046dc16d00f6b8ea4221322583
SathvikaKurli <92846635+SathvikaKurli@users.noreply.github.com>
2021-10-29 12:07:23 +0530

    Assignment1

---
+++ firstAssignments/src/Solution4.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution4 {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+    String A=sc.next();
+    /* Enter your code here. Print output to STDOUT. */
+    if(A.equalsIgnoreCase(new StringBuffer(A).reverse().toString()))
+        System.out.println("Yes");
+    else
+        System.out.println("No");
+}
+}
