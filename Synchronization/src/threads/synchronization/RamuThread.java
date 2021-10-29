ca2f77611a31145bb03fb9be3c29391453ddbf71
SathvikaKurli <92846635+SathvikaKurli@users.noreply.github.com>
2021-10-29 12:12:20 +0530

    Synchronization

---
+++ Synchronization/src/threads/synchronization/RamuThread.java
@@ -0,0 +1,13 @@
+package threads.synchronization;
+
+public class RamuThread  extends Thread{
+	Table mTable;
+public  RamuThread(Table table) {
+		mTable = table;
+	}
+	public void run() {
+		//TODO Auto-generated method stub
+		super.run();
+		mTable.printTable(5);
+	}
+}
